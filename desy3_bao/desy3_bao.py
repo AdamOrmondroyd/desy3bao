c5fba3636f3e6a3ed3e5d4dfc33534679fb5af15
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-05-08 12:20:30 +0100

    add structure to likelihood

--- desy3_bao/desy3_bao.py
+++ desy3_bao/desy3_bao.py
@@ -2,4 +2,11 @@ from cobaya.likelihood import Likelihood
 
 
 class DESY3BAO(Likelihood):
-    pass
+    def initialize(self):
+        pass
+
+    def get_requirements(self):
+        pass
+
+    def logp(self, **params_values):
+        pass
